5d15dbd888c76d924410b9ac1f8fd76459d5c022
m_kawakami <dlold1962@gmail.com>
2022-11-09 04:37:10 +0900

    implement code editor

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -1,14 +1,51 @@
 <template>
   <div>
-    <h3>Code Editor</h3>
+    <PrismEditor
+      class="my-editor height-200"
+      v-model="code"
+      :highlight="highlighter"
+      line-numbers
+    >
+    </PrismEditor>
   </div>
 </template>
 <script>
+// import Prism Editor
+import { PrismEditor } from 'vue-prism-editor';
+import 'vue-prism-editor/dist/prismeditor.min.css';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/themes/prism-tomorrow.css';
+
 export default {
+  components: {
+    PrismEditor
+  },
   data () {
-    return {};
+    return {
+      code: 'console.log("Hello World")'
+    };
   },
   props: {},
-  methods: {}
+  methods: {
+    highlighter(code) {
+      return highlight(code, languages.js);
+    }
+  }
 }
 </script>
+
+<style>
+.my-editor {
+  background: #2d2d2d;
+  color: #ccc;
+  font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+  font-size: 14px;
+  line-height: 1.5;
+  padding: 5px;
+}
+.prism-editor__textarea:focus {
+  outline: none;
+}
+</style>
--- src/views/ExamView.vue
+++ src/views/ExamView.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
     <h3>Exams</h3>
+    <PrismEditor/>
   </div>
 </template>
 <script>
+import PrismEditor from '@/components/CodeEditor';
 export default {
+  components: {
+    PrismEditor
+  },
   data () {
     return {};
   },
