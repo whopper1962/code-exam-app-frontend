c2259b1660f8468be93b911954b3196929767a88
Masashi Kawakami <dlold1962@gmail.com>
2022-11-10 06:45:55 +0900

    implement header style

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
   <div id="app">
-    <router-view/>
+    <HeaderView/>
+    <div class="main-content">
+      <router-view/>
+    </div>
   </div>
 </template>
+
+<script>
+import HeaderView from '@/components/HeaderView';
+
+export default {
+  components: {
+    HeaderView
+  }
+}
+</script>
 
 <style>
 #app {
@@ -11,5 +24,12 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+body {
+  padding: 0;
+  margin: 0;
+}
+.main-content {
+  margin: 10px;
 }
 </style>
--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -1,4 +1,23 @@
 <template>
+<div class="topnav">
+  <router-link
+    class="main-title"
+    :to="{
+      name: 'WELCOME_VIEW'
+    }"
+  >
+    Code-Exam
+  </router-link>
+  <router-link
+    v-for="(item, index) in headerItems"
+    :key="`item_${index}`"
+    :to="{
+      name: item.name
+    }"
+  >
+    {{ item.label }}
+  </router-link>
+</div>
 </template>
 
 <script>
@@ -6,9 +25,44 @@ export default {
   data () {
     return {};
   },
+  computed: {
+    headerItems () {
+      return [
+        {
+          name: 'EXAM_VIEW',
+          label: 'Exam'
+        }
+      ];
+    }
+  },
   methods: {}
 }
 </script>
 
 <style scoped>
+.topnav {
+  margin: 0;
+  overflow: hidden;
+  background-color: #333;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 20px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
 </style>
